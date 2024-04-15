b532d91bf78ebc94525b3a2631fe53a349324631
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 17:24:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Q120
+var likemovies = ['The Godfather', 'The Shawshank Redemption', 'The Dark Knight', 'The Godfather: Part II', 'The Lord of the Rings: The Return of the King'];
+// use s for.. loop to iteral therough  
+for (var _i = 0, likemovies_1 = likemovies; _i < likemovies_1.length; _i++) {
+    var movie = likemovies_1[_i];
+    console.log(movie);
+}
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q120
+const likemovies : string[] = ['The Godfather', 'The Shawshank Redemption', 'The Dark Knight', 'The Godfather: Part II', 'The Lord of the Rings: The Return of the King'];
+// use s for.. loop to iteral therough  
+for (const movie of likemovies ){
+    console.log(movie);
+}
